3df32d2df6b00168c1851983e13362c72c2e0441
Aviansh Reddy <>
2020-06-27 17:16:00 +0530

    wip: new dataset type

--- model.py
+++ model.py
@@ -1,22 +1,120 @@
 import os
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, IterableDataset
 
-class NumbersDataset(Dataset):
-    def __init__(self, low, high):
+
+class MyIterableDataset(IterableDataset):
+    def __init__(self, data):
         super().__init__()
-        self.samples = list(range(low,high))
+        self.data = data
+    
+    def __iter__(self):
+        return iter(self.data)
+
+iterable_dataset = MyIterableDataset(range(1,1000))
+
+loader = DataLoader(iterable_dataset, batch_size = 4)
+
+for batch in loader:
+    print(batch)
+
+
+
+
+
+
+
+
+
+
+
+class MyMapDataset(Dataset):
+    def __init__(self, data):
+        super().__init__()
+        self.data = data
 
     def __len__(self):
-        return len(self.samples)
-    
+        return len(self.data)
+
     def __getitem__(self, idx):
-        return self.samples[idx]
+        return self.data[idx]
+
+map_dataset = MyMapDataset(data=range(1,1000))
+
+loader = DataLoader(map_dataset, batch_size = 1000)
+
+# for batch in loader:
+    # print(batch)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class NumbersDataset(Dataset):
+#     def __init__(self, low, high):
+#         super().__init__()
+#         self.samples = list(range(low,high))
+
+#     def __len__(self):
+#         return len(self.samples)
+    
+#     def __getitem__(self, idx):
+#         return self.samples[idx]
 
     
-if __name__ == "__main__":
-    ds = NumbersDataset(1,100)
-    print(len(ds))
-    print(ds[10])
-    print(ds[11:18])
+# if __name__ == "__main__":
+#     ds = NumbersDataset(1,100)
+#     print(len(ds))
+#     print(ds[10])
+#     print(ds[11:18])
 
         
