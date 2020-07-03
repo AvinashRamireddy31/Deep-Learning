d5d1cc857727c3281ad48b209d7c86abf7454c47
Aviansh Reddy <>
2020-07-03 17:50:10 +0530

    ...

--- youtube_download.py
+++ youtube_download.py
@@ -4,11 +4,12 @@ from pytube import YouTube
 def downloadYouTube(videourl, path, filename):
 
     yt = YouTube(videourl)
-    yt = yt.streams.filter(progressive= True, file_extension='mp4').order_by('resolution').desc().first()
+    yt = yt.streams.filter(progressive= False, file_extension='mp4').order_by('resolution').desc().first()
     if not os.path.exists(path):
         os.makedirs(path)
     yt.download(path, filename)
+     
 
 videourl = 'https://www.youtube.com/watch?v=_gPcYovP7wc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=7'
  
-downloadYouTube(videourl, path='./videos/Dynamic_Programming', filename='7 Test Subset Sum Problem')
+downloadYouTube(videourl, path='./videos/Dynamic_Programming', filename='7 Subset Sum Problem')
