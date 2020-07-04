f0a3d4d256b9b5c2a47a5f3c9b946f9342dfb4c7
Aviansh Reddy <>
2020-07-04 21:26:02 +0530

    ...

--- dynamic_programming/climbing_stairs.py
+++ dynamic_programming/climbing_stairs.py
@@ -1,34 +1,21 @@
 # 746. Min Cost Climbing Stairs
 
-class Solution:
-    
-    def minCost_2(self, cost, n): 
-        #Base case
-        if n == 1:
-            return cost[0]
-        #Base case
-        if n == 2:
-            return min(cost[0], cost[1]) 
-        
-        first = cost[n-1] + self.minCost(cost, n-1)
-        second = cost[n-2] + self.minCost(cost, n-2)
-        
-        return min(first, second)
-
+class Solution: 
     def minCost(self, cost, n): 
-        #Base case 
-        if n
+       
+        dp = [None]*n
+        
+        dp[0] = cost[0]
+        dp[1] = cost[1] 
 
+        for i in range(2, n):
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
 
-
-
-
-        return -1
- 
+        return min(dp[n-1], dp[n-2]) 
     
     def minCostClimbingStairs(self, cost: [int]) -> int:
         return self.minCost(cost, len(cost))
 
 sol = Solution()
-result = sol.minCostClimbingStairs(cost = [10, 15, 20])
+result = sol.minCostClimbingStairs(cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1])
 print(result)
